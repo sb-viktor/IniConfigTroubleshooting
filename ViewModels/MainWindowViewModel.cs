0a865c798596562b1e539dc3bb1c17dbef79d856
Viktor Slobodianyk <viktor-sbdn@proton.me>
2025-03-06 21:09:28 +0100

    Adding Commadns to ViewModel
    - Cut
    - Copy
    - Paste
    - SelectAll
    - Undo

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,18 @@
-﻿namespace IniConfigTroubleshooting.ViewModels;
+﻿using AvaloniaEdit.Editing;
+using AvaloniaEdit;
+
+namespace IniConfigTroubleshooting.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    public void CopyMouseCommand(TextArea textArea) => ApplicationCommands.Copy.Execute(null, textArea);
+
+    public void CutMouseCommand(TextArea textArea) => ApplicationCommands.Cut.Execute(null, textArea);
+
+    public void PasteMouseCommand(TextArea textArea) => ApplicationCommands.Paste.Execute(null, textArea);
+
+    public void SelectAllMouseCommand(TextArea textArea) => ApplicationCommands.SelectAll.Execute(null, textArea);
+
+    // Undo Status is not given back to disable it's item in ContextFlyout; therefore it's not being used yet.
+    public void UndoMouseCommand(TextArea textArea) => ApplicationCommands.Undo.Execute(null, textArea);
 }
