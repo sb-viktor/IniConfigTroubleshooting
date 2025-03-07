284e85b89f7ff1b60d52f1bec8ced662c23222fc
Viktor Slobodianyk <viktor-sbdn@proton.me>
2025-03-07 21:22:03 +0100

    Format Commands & Add Regions

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -10,21 +10,36 @@ namespace IniConfigTroubleshooting.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public void CopyMouseCommand(TextArea textArea) => ApplicationCommands.Copy.Execute(null, textArea);
-
-    public void CutMouseCommand(TextArea textArea) => ApplicationCommands.Cut.Execute(null, textArea);
-
-    public void PasteMouseCommand(TextArea textArea) => ApplicationCommands.Paste.Execute(null, textArea);
-
-    public void SelectAllMouseCommand(TextArea textArea) => ApplicationCommands.SelectAll.Execute(null, textArea);
-
-    // Undo Status is not given back to disable it's item in ContextFlyout; therefore it's not being used yet.
-    public void UndoMouseCommand(TextArea textArea) => ApplicationCommands.Undo.Execute(null, textArea);
-
-    // Redo Status is not given back to disable it's item in ContextFlyout; therefore it's not being used yet.
-    public void RedoMouseCommand(TextArea textArea) => ApplicationCommands.Redo.Execute(null, textArea);
-
+    #region Fields
     [ObservableProperty] private TextDocument? _sourceDocument;
+    #endregion
+
+    #region Commands
+    [RelayCommand]
+    private void CopyMouse(TextArea textArea)
+        => ApplicationCommands.Copy.Execute(null, textArea);
+
+    [RelayCommand]
+    private void CutMouseCommand(TextArea textArea)
+        => ApplicationCommands.Cut.Execute(null, textArea);
+
+    [RelayCommand]
+    private void PasteMouseCommand(TextArea textArea)
+        => ApplicationCommands.Paste.Execute(null, textArea);
+
+    [RelayCommand]
+    private void SelectAllMouseCommand(TextArea textArea)
+        => ApplicationCommands.SelectAll.Execute(null, textArea);
+
+    // Undo Status is not given back to disable its item in ContextFlyout; therefore it's not being used yet.
+    [RelayCommand]
+    private void UndoMouseCommand(TextArea textArea)
+        => ApplicationCommands.Undo.Execute(null, textArea);
+
+    // Redo Status is not given back to disable its item in ContextFlyout; therefore it's not being used yet.
+    [RelayCommand]
+    private void RedoMouseCommand(TextArea textArea)
+        => ApplicationCommands.Redo.Execute(null, textArea);
 
     [RelayCommand]
     private async Task OpenFile(CancellationToken token)
@@ -39,4 +54,5 @@ public partial class MainWindowViewModel : ViewModelBase
         using var reader = new StreamReader(readStream);
         SourceDocument = new TextDocument(await reader.ReadToEndAsync(token));
     }
+    #endregion
 }
