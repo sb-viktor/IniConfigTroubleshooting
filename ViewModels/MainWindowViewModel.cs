e1f79d1741fdbded81bebbb91f26ceed902d2006
Viktor Slobodianyk <viktor-sbdn@proton.me>
2025-03-07 10:40:46 +0100

    Create OpenFileCommand & ReadAsync to TextDocument

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using AvaloniaEdit.Editing;
 using AvaloniaEdit;
+using CommunityToolkit.Mvvm.Input;
+using IniConfigTroubleshooting.Services;
+using Microsoft.Extensions.DependencyInjection;
+using CommunityToolkit.Mvvm.ComponentModel;
+using AvaloniaEdit.Document;
 
 namespace IniConfigTroubleshooting.ViewModels;
 
@@ -15,4 +20,20 @@ public partial class MainWindowViewModel : ViewModelBase
 
     // Undo Status is not given back to disable it's item in ContextFlyout; therefore it's not being used yet.
     public void UndoMouseCommand(TextArea textArea) => ApplicationCommands.Undo.Execute(null, textArea);
+
+    [ObservableProperty] private TextDocument? _sourceDocument;
+
+    [RelayCommand]
+    private async Task OpenFile(CancellationToken token)
+    {
+        var filesService = App.Current?.Services?.GetService<IFilesService>();
+        if (filesService is null) throw new NullReferenceException("Missing File Service instance.");
+
+        var file = await filesService.OpenFileAsync();
+        if (file is null) return;
+
+        await using var readStream = await file.OpenReadAsync();
+        using var reader = new StreamReader(readStream);
+        SourceDocument = new TextDocument(await reader.ReadToEndAsync(token));
+    }
 }
