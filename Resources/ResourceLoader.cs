795de616c52322ae7581010ad97042425021ed34
Viktor Slobodianyk <viktor-sbdn@proton.me>
2025-03-09 20:59:36 +0100

    Create Syntax Highlighter
    & Add ResourceLoader

---
+++ Resources/ResourceLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace IniConfigTroubleshooting.Resources;
+
+internal class ResourceLoader
+{
+    const string SampleFilesPrefix = "IniConfigTroubleshooting.Resources.SampleFiles.";
+
+    internal static string LoadSampleFile(string fileName)
+    {
+        Stream stream = typeof(ResourceLoader).GetTypeInfo().Assembly.GetManifestResourceStream(SampleFilesPrefix + fileName);
+
+        if (stream == null)
+            return string.Empty;
+
+        using (stream)
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+}
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,10 @@
 using Avalonia.Controls;
+using AvaloniaEdit;
+using AvaloniaEdit.Document;
+using AvaloniaEdit.TextMate;
+using IniConfigTroubleshooting.Resources;
+using System.Resources;
+using TextMateSharp.Grammars;
 
 namespace IniConfigTroubleshooting.Views;
 
@@ -7,5 +13,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        TextEditor? TextEditor = this.FindControl<TextEditor>("Editor");
+
+        var RegistryOptions = new RegistryOptions(ThemeName.Light);
+
+        var TextMateInstallation = TextEditor.InstallTextMate(RegistryOptions);
+
+        Language IniFileLanguage = RegistryOptions.GetLanguageByExtension(".ini");
+
+        //string scopeName = RegistryOptions.GetScopeByLanguageId(IniFileLanguage.Id);
+
+        //TextEditor.Document = new TextDocument(ResourceLoader.LoadSampleFile(scopeName));
+
+        TextMateInstallation.SetGrammar(RegistryOptions.GetScopeByLanguageId(IniFileLanguage.Id));
+
     }
 }
