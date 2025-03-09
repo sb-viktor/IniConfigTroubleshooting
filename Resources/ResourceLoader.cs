5d86eba0dcf20f0bb920f0d4218c9cb620a1b579
Viktor Slobodianyk <viktor-sbdn@proton.me>
2025-03-09 21:13:23 +0100

    Comment All RecourceLoader
    Dont realy need, add only cause https://github.com/danipen/TextMateSharp use it

--- Resources/ResourceLoader.cs
+++ Resources/ResourceLoader.cs
@@ -3,21 +3,21 @@ using System.Reflection;
 
 namespace IniConfigTroubleshooting.Resources;
 
-internal class ResourceLoader
-{
-    const string SampleFilesPrefix = "IniConfigTroubleshooting.Resources.SampleFiles.";
+//internal class ResourceLoader
+//{
+//    const string SampleFilesPrefix = "IniConfigTroubleshooting.Resources.SampleFiles.";
 
-    internal static string LoadSampleFile(string fileName)
-    {
-        Stream stream = typeof(ResourceLoader).GetTypeInfo().Assembly.GetManifestResourceStream(SampleFilesPrefix + fileName);
+//    internal static string LoadSampleFile(string fileName)
+//    {
+//        Stream stream = typeof(ResourceLoader).GetTypeInfo().Assembly.GetManifestResourceStream(SampleFilesPrefix + fileName);
 
-        if (stream == null)
-            return string.Empty;
+//        if (stream == null)
+//            return string.Empty;
 
-        using (stream)
-        using (StreamReader reader = new StreamReader(stream))
-        {
-            return reader.ReadToEnd();
-        }
-    }
-}
+//        using (stream)
+//        using (StreamReader reader = new StreamReader(stream))
+//        {
+//            return reader.ReadToEnd();
+//        }
+//    }
+//}
