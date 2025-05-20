f2ba5750629509bdf937bd0ecaccc552362901d5
sb-viktor <viktor-sbdn@proton.me>
2025-05-20 13:26:45 +0200

    Comment out unused menu items and editor control in MainWindow.axaml; clean up MainWindow.axaml.cs by removing unnecessary using directives and initialization code.

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,6 +1,4 @@
 ﻿using Avalonia.Controls;
-using AvaloniaEdit;
-using IniConfigTroubleshooting.ViewModels;
 
 namespace IniConfigTroubleshooting.Views;
 
@@ -9,10 +7,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        var viewModel = new MainWindowViewModel();
-        DataContext = viewModel;
-        var editor = this.FindControl<TextEditor>("Editor");
-        if (editor is not null)
-            viewModel.AttachEditor(editor);
     }
 }
